6b92be8e39cd589b2c283ba8508a43579d353698
Evgeni <evgeni@leapdao.org>
2021-04-30 16:27:13 +0200

    add Q2T deposit

--- contracts/Q2T.sol
+++ contracts/Q2T.sol
@@ -1,22 +1,57 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.7.4;
+pragma experimental ABIEncoderV2;
 
 import "./AddressesProvider.sol";
-import "./TemplateTreasury.sol";
+import "./TemplatesTreasuries.sol";
 import "./DataTypes.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155Holder.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./ILendingPool.sol";
+import "./IPriceOracle.sol";
 
-contract Q2T {
+contract Q2T is ERC1155Holder {
+    using SafeMath for uint256;
+
+    event ThresholdReached(uint256 _id);
+    event Deposited(
+        DataTypes.Template _template, 
+        address _depositor, 
+        string _currency, 
+        uint256 _amount,
+        uint256 _repaymentAmount,
+        uint256 _aTokensAmount
+    );
+    event Delegated(
+        DataTypes.Template _template,
+        address _communityTreasury, 
+        address _currency, 
+        uint256 _amount
+    );
+
     address public addressesProvider;
-    address public templateRepayerTreasuryAddress;
-    address public templateTreasuryAddress;
-    mapping (DataTypes.Template => uint256) public templateRepayerTreasuryNFTs;
-    mapping (DataTypes.Template => uint256) public templateTreasuryNFTs;
+    address public templatesReapayersTreasuries;
+    address public templatesTreasuries;
     uint256 public totalQ2TFund;
     uint256 public totalRepayerFund;
+    mapping (address => mapping (address => uint256)) public repaymentAmounts;
+    mapping (address => uint256) public depositors;
 
-    function deposit(DataTypes.Template _template, uint256 _amount, uint256 _repayment) public override {
+    constructor(
+        address _addressesProvider
+    ) {
+        require(_addressesProvider != address(0), "Addressess provider cannot be 0");
+
+        addressesProvider = _addressesProvider;
+
+        TemplatesTreasuries templatesTreasuriesContract = new TemplatesTreasuries("");
+        templatesTreasuries = address(templatesTreasuriesContract);
+        TemplatesTreasuries templatesReapayersTreasuriesContract = new TemplatesTreasuries("");
+        templatesReapayersTreasuries = address(templatesReapayersTreasuriesContract);
+    }
+
+    function deposit(DataTypes.Template _template, uint256 _amount, uint256 _repayment) public {
         address currencyAddress = AddressesProvider(addressesProvider).currenciesAddresses("DAI");
         
         IERC20 currency = IERC20(currencyAddress);
@@ -28,27 +63,60 @@ contract Q2T {
         
         currency.transferFrom(msg.sender, address(this), _amount);
 
-        uint256 q2tAmount = _amount.sub(_repayment);
-
-        if(templateTreasuryNFTs[_template] == 0) {
-            templateTreasuryNFTs[_template] = TemplateTreasury(templateTreasuryAddress).mint(_amount);
-        }
-
-        if(templateRepayerTreasuryNFTs[_template] == 0) {
-            templateRepayerTreasuryNFTs[_template] = TemplateTreasury(templateRepayerTreasuryAddress).mint(_repayment);
-        }
-
-
+        uint256 repaymentAmount = _amount.mul(_repayment).div(100);
+        uint256 q2tAmount = _amount.sub(repaymentAmount);
         
-        ILendingPool lendingPool = ILendingPool(aaveProtocolDataProvider.ADDRESSES_PROVIDER().getLendingPool());
+        ILendingPool lendingPool = ILendingPool(
+            ILendingPoolAddressesProvider(
+                AddressesProvider(addressesProvider).lendingPoolAP()).getLendingPool());
 
-        currency.transferFrom(msg.sender, address(this), amount);
-        currency.approve(address(lendingPool), amount);
-        lendingPool.deposit(currencyAddress, amount, address(this), 0);
-        repayableAmount[msg.sender][currencyAddress] = repayableAmount[msg.sender][currencyAddress].add(amount.mul(_repayment).div(100));
-        depositors[msg.sender] = depositors[msg.sender].add(amount);
-        totalDeposited = totalDeposited.add(amount);
+        IERC20 aToken = IERC20(lendingPool.getReserveData(currencyAddress).aTokenAddress);
+        uint256 aTokenBalanceBefore = aToken.balanceOf(address(this));
 
-        emit Deposited(msg.sender, _currency, _amount);
+        currency.approve(address(lendingPool), q2tAmount);
+        lendingPool.deposit(currencyAddress, q2tAmount, address(this), 0);
+
+        uint256 aTokenReceived = aToken.balanceOf(address(this)).sub(aTokenBalanceBefore);
+
+        if(TemplatesTreasuries(templatesTreasuries).balanceOf(address(this), uint256(_template)) == 0) {
+            TemplatesTreasuries(templatesTreasuries).mint(_template, aTokenReceived);
+        } else {
+            TemplatesTreasuries(templatesTreasuries).addFunds(_template, aTokenReceived);
+        }
+
+        if(TemplatesTreasuries(templatesReapayersTreasuries).balanceOf(address(this), uint256(_template)) == 0) {
+            TemplatesTreasuries(templatesReapayersTreasuries).mint(_template, repaymentAmount);
+        } else {
+            TemplatesTreasuries(templatesReapayersTreasuries).addFunds(_template, repaymentAmount);
+        }
+
+        repaymentAmounts[msg.sender][currencyAddress] = repaymentAmounts[msg.sender][currencyAddress].add(repaymentAmount);
+        depositors[msg.sender] = depositors[msg.sender].add(q2tAmount);
+
+        emit Deposited(_template, msg.sender, "DAI", _amount, repaymentAmount, aTokenReceived);
+    }
+
+    function thresholdReached(uint256 _id) public override {
+        require(msg.sender == communityTreasuries[_id], "wrong id");
+        require(isTreasuryActive[msg.sender], "treasury is not active");
+
+        IPriceOracle priceOracle = IPriceOracle(
+            ILendingPoolAddressesProvider(
+                AddressesProvider(addressesProvider).lendingPoolAP()).getPriceOracle());
+        ILendingPool lendingPool = ILendingPool(
+            ILendingPoolAddressesProvider(
+                AddressesProvider(addressesProvider).lendingPoolAP()).getLendingPool());
+
+        //delegation is for usdc for now
+        (,,uint256 borrowingPower,,,) = lendingPool.getUserAccountData(address(this));
+        uint256 totalDeligating = borrowingPower.div(
+            priceOracle.getAssetPrice(
+                AddressesProvider(addressesProvider).currenciesAddresses("USDC")));
+        totalDeligating = totalDeligating.mul(80).div(100); //lower borrowing a bit to avoid liquidations
+
+        //quadratic distribution delegation to different communities
+        _distribute(totalDeligating.mul(1e18));
+
+        emit ThresholdReached(_id);
     }
 }
---
+++ contracts/TemplatesTreasuries.sol
@@ -0,0 +1,74 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.7.4;
+
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155MetadataURI.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./DataTypes.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract TemplatesTreasuries is ERC1155 {
+    using SafeMath for uint256;
+    using Counters for Counters.Counter;
+
+    event TreasuryFunded (
+        DataTypes.Template _template,
+        uint256 _amount
+    );
+
+    event TreasuryFundsAdded (
+        DataTypes.Template _template,
+        uint256 _amount,
+        uint256 _newFund
+    );
+
+    event TreasuryBurnt (
+        DataTypes.Template _template,
+        uint256 _finalFund
+    );
+
+    mapping(DataTypes.Template => Counters.Counter) private treasuryCounters;
+    address public q2t;
+    mapping (DataTypes.Template => uint256[]) public funds;
+
+    constructor(string memory _uri) ERC1155(_uri) {
+        q2t = msg.sender;
+    }
+
+    function mint(DataTypes.Template _template, uint256 _amount) public returns (uint256) {
+        require(msg.sender == q2t, "Only Q2T can mint");
+        require(_amount > 0, "Amount cant be 0");
+        require(balanceOf(q2t, uint256(_template)) == 0, "Template already has token");
+
+        _mint(q2t, uint256(_template), 1, "");
+        funds[_template].push(_amount);
+
+        emit TreasuryFunded(_template, _amount);
+    }
+
+    function addFunds(DataTypes.Template _template, uint256 _amount) public {
+        require(msg.sender == q2t, "Only Q2T can add funds");
+        require(balanceOf(q2t, uint256(_template)) == 1, "Template token not minted");
+        require(_amount > 0, "Amount cant be 0");
+
+        uint256 currentId = funds[_template].length - 1;
+        
+        funds[_template][currentId] = funds[_template][currentId].add(_amount);
+
+        emit TreasuryFundsAdded(_template, _amount, funds[_template][currentId]);
+    }
+
+    function burn(DataTypes.Template _template) public {
+        require(msg.sender == q2t, "Only Q2T can burn treasury");
+        require(balanceOf(q2t, uint256(_template)) == 1, "Template token not minted");
+
+        uint256 fund = funds[_template][funds[_template].length - 1];
+
+        _burn(q2t, uint256(_template), 1);
+
+        emit TreasuryBurnt(_template, fund);
+    }
+}
